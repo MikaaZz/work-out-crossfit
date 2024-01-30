8ae7797180eac25800304880eace342e287e1c69
Mikhael Zimmermann <mikhael928@gmail.com>
2024-01-29 22:30:38 -0300

    Another try to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,7 @@ import { getServerSession } from "next-auth";
 import SessionProvider from "./components/SessionProvider/SessionProvider";
 import NavMenu from "./components/NavMenu/NavMenu";
 
-import { Poppins as FontSans } from "next/font/google";
-
 import { cn } from "@/lib/utils";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-  weight: "300",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,12 +20,7 @@ export default async function RootLayout({
 
   return (
     <html lang="pt-br">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <SessionProvider session={session}>
           <main className={cn("flex items-start justify-start bg-slate-800")}>
             <NavMenu />
